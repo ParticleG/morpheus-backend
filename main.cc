b7d279d2c2ef76cb93b29d5a5cc534ab910f32cb
ParticleG <particle_g@outlook.com>
2022-10-11 23:00:56 +0800

    - Update sources

--- main.cc
+++ main.cc
@@ -1,7 +1,76 @@
-#include <drogon/drogon.h>
+//#include <drogon/drogon.h>
+//
+//int main() {
+//    drogon::app().loadConfigFile("config.json");
+//    drogon::app().run();
+//    return 0;
+//}
+
+#define PY_SSIZE_T_CLEAN
+
+#include <Python.h>
+#include <string>
 
 int main() {
-    drogon::app().loadConfigFile("config.json");
-    drogon::app().run();
+    std::string file = "greet",
+            function = "square";
+    int32_t number = 5;
+
+    PyObject *pName, *pModule, *pFunc;
+    PyObject *pArgs, *pValue;
+
+    Py_Initialize();
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append('./')");
+    pName = PyUnicode_DecodeFSDefault(file.c_str());
+    /* Error checking of pName left out */
+
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    if (pModule != nullptr) {
+        pFunc = PyObject_GetAttrString(pModule, function.c_str());
+        /* pFunc is a new reference */
+
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pArgs = PyTuple_New(1);
+            for (int i = 0; i < 1; ++i) {
+                pValue = PyLong_FromLong(number);
+                if (!pValue) {
+                    Py_DECREF(pArgs);
+                    Py_DECREF(pModule);
+                    fprintf(stderr, "Cannot convert argument\n");
+                    return 1;
+                }
+                /* pValue reference stolen here: */
+                PyTuple_SetItem(pArgs, i, pValue);
+            }
+            pValue = PyObject_CallObject(pFunc, pArgs);
+            Py_DECREF(pArgs);
+            if (pValue != nullptr) {
+                printf("Result of call: %ld\n", PyLong_AsLong(pValue));
+                Py_DECREF(pValue);
+            } else {
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr, "Call failed\n");
+                return 1;
+            }
+        } else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", function.c_str());
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    } else {
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\"\n", file.c_str());
+        return 1;
+    }
+    if (Py_FinalizeEx() < 0) {
+        return 120;
+    }
     return 0;
 }
